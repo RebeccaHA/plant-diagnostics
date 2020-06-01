c6d0fd2ccf8fc5f2c2308301e33f6d3ff1c066fb
RebeccaHA <rebeccahunt034@gmail.com>
2020-06-01 18:47:30 +0100

    set up table attributes for plants, queries, diagnoses and users

--- db/migrate/20200601174155_create_queries.rb
+++ db/migrate/20200601174155_create_queries.rb
@@ -1,7 +1,10 @@
 class CreateQueries < ActiveRecord::Migration[6.0]
   def change
     create_table :queries do |t|
-
+      t.string :question
+      t.string :image_url
+      t.integer :user_id
+      t.integer :plant_id
       t.timestamps
     end
   end
--- db/migrate/20200601174220_create_diagnoses.rb
+++ db/migrate/20200601174220_create_diagnoses.rb
@@ -1,7 +1,9 @@
 class CreateDiagnoses < ActiveRecord::Migration[6.0]
   def change
     create_table :diagnoses do |t|
-
+      t.string :comment
+      t.integer :user_id
+      t.integer :query_id
       t.timestamps
     end
   end
