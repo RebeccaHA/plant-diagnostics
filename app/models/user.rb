3b12ce0472b7d6185516646cd060bac9351a493d
RebeccaHA <rebeccahunt034@gmail.com>
2020-06-01 19:59:02 +0100

    added resources routes for users, plants and diagnoses. Nested resources for plants and session login/logout routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :queries
     has_many :plants, through: :queries
     has_many :diagnosis
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :diagnoses
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  
+  resources :queries do
+    resources :plants, only: [:index, :show, :new, :edit]
+  end
+  
+  resources :plants, only: [:index, :show, :new, :edit]
+
+  root 'static#home'
+
+
 end
