6eb9d26f6896d781f511b135eb7ae1a74b0396d4
RebeccaHA <rebeccahunt034@gmail.com>
2020-06-04 17:55:28 +0100

    added view error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
     has_many :plants, through: :queries
     has_many :diagnoses
     has_secure_password
+    validates :password, presence: true
+    validates :name, uniqueness: true
+    
 end
