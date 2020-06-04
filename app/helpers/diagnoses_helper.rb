a7c35e00bddff4ae072acfa83b6fb00b83b17387
RebeccaHA <rebeccahunt034@gmail.com>
2020-06-04 11:44:43 +0100

    added diagnosis on show page in the view so can test other things

--- app/helpers/diagnoses_helper.rb
+++ app/helpers/diagnoses_helper.rb
@@ -1,13 +1,13 @@
 module DiagnosesHelper
-    def display_diagnosis
-        if @query.diagnoses
-            @query.diagnoses.each do |diagnosis|
-            diagnosis.comment
-            diagnosis.created_at
-            diagnosis.user.name
-            end
-        end
-    end
+    # def display_diagnosis
+    #     if @query.diagnoses
+    #         @query.diagnoses.each do |diagnosis|
+    #         diagnosis.comment
+    #         diagnosis.created_at
+    #         diagnosis.user.name
+    #         end
+    #     end
+    # end
 
    
 end
