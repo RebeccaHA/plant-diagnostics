11bf53411e59a93137ceaaf9045f0d6d39dfe212
RebeccaHA <rebeccahunt034@gmail.com>
2020-06-11 18:17:06 +0100

    tidied indenting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,31 +1,31 @@
 class SessionsController < ApplicationController
     def new
-      @user = User.new
+    @user = User.new
     end
 
     def create
     user = User.find_by(name: params[:user][:name])
-      if user && user.authenticate(params[:user][:password])
-        session[:user_id] = user.id
-        redirect_to user_path(user)
-      else
-        redirect_to '/login'
-      end
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            redirect_to '/login'
+        end
     end
 
     def destroy
-      session.delete(:user_id)
-      redirect_to '/login'
+    session.delete(:user_id)
+    redirect_to '/login'
     end
 
     def google_oauth2
     @user = User.find_or_create_by_auth(auth)
-      if @user.save
-        session[:user_id] = @user.id
-        redirect_to user_path(@user)
-      else
-        redirect_to '/login'
-      end 
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to '/login'
+        end 
     end
 
     def auth
