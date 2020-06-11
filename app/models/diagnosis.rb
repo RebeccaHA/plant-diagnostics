2a50f16fe8815c9b08a62e21e4051244a64c77c6
RebeccaHA <rebeccahunt034@gmail.com>
2020-06-11 18:18:06 +0100

    tidied indent in plant controller

--- app/models/diagnosis.rb
+++ app/models/diagnosis.rb
@@ -2,7 +2,5 @@ class Diagnosis < ApplicationRecord
     belongs_to :user
     belongs_to :query
     validates :comment, presence: true
-
-
     scope :best_diagnoses, -> (query_id) {where('query_id = ?', query_id).order(upvote: :desc)}
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -3,8 +3,8 @@ class Plant < ApplicationRecord
     has_many :users, through: :queries
     validates :name, presence: true
 
-   def self.search(parameter)
-    self.where("lower(name) LIKE :search", search: "%#{parameter}%")  
-   end
+    def self.search(parameter)
+        self.where("lower(name) LIKE :search", search: "%#{parameter}%")  
+    end
  
 end
